a59978695b6f9dd5c56af00fe067effe02f2bbf8
Mishal Shah <mishal_shah@apple.com>
2022-09-13 00:57:38 -0700

    Fix the ci_test script

--- ci_test.py
+++ ci_test.py
@@ -65,15 +65,17 @@ def main():
     dockerfiles = get_dockerfiles()
     for dockerfile in dockerfiles:
         docker_dir = os.path.dirname(os.path.realpath(__file__))
+        if "windows" in dockerfile:
+            continue
         print("Testing {}".format(dockerfile))
         sys.stdout.flush()
         log_file = dockerfile.replace(docker_dir,"").replace("/", "_")
         log_file = "{}.log".format(log_file)
-        cmd = "docker build --no-cache=true {}".format(dockerfile)
+        cmd = "docker build --no-cache=true -build-arg http_proxy={} --build-arg https_proxy={} {}".format(os.environ['http_proxy'], os.environ['https_proxy'], dockerfile)
         if "buildx" in dockerfile:
             # if "buildx" is part of the path, we want to use the new buildx build system and build
             # for both amd64 and arm64.
-            cmd = "docker buildx build --platform linux/arm64,linux/amd64 --no-cache=true {}".format(dockerfile)
+            cmd = "docker buildx build --platform linux/arm64,linux/amd64 --no-cache=true --driver-opt env.http_proxy={} --driver-opt env.https_proxy={} {}".format(os.environ['http_proxy'], os.environ['https_proxy'], dockerfile)
         status = run_command(cmd, log_file)
         results[dockerfile] = status
         if status != 0:
