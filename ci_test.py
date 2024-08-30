f9be7230b6bb84f00abb680d494b1bffa39c5a72
Mishal Shah <shahmishal@users.noreply.github.com>
2024-08-30 10:29:09 -0700

    Rename swift-ci folder to main (#407)
    * Move the folder from master -> main
    
    * Remove .github/.DS_Store
    
    * Add support for logs in GitHub Actions
    
    * Prune after building docker images

--- ci_test.py
+++ ci_test.py
@@ -69,15 +69,15 @@ def main():
         sys.stdout.flush()
         log_file = dockerfile.replace(docker_dir,"").replace("/", "_")
         log_file = "{}.log".format(log_file)
-        cmd = "docker build --no-cache=true --build-arg http_proxy={} --build-arg https_proxy={} {}".format(os.environ['http_proxy'], os.environ['https_proxy'], dockerfile)
+        cmd = f"docker build --no-cache=true {dockerfile}"
         if "buildx" in dockerfile:
             # if "buildx" is part of the path, we want to use the new buildx build system and build
             # for both amd64 and arm64.
-            cmd = "docker buildx create --use --driver-opt env.http_proxy={} --driver-opt env.https_proxy={}".format(os.environ['http_proxy'], os.environ['https_proxy'])
+            cmd = "docker buildx create --use"
             run_command(cmd, log_file)
             cmd = "docker buildx inspect --bootstrap"
             run_command(cmd, log_file)
-            cmd = "docker buildx build --platform linux/arm64,linux/amd64 --no-cache=true --build-arg env.http_proxy={} --build-arg env.https_proxy={} {}".format(os.environ['http_proxy'], os.environ['https_proxy'], dockerfile)
+            cmd = f"docker buildx build --platform linux/arm64,linux/amd64 --no-cache=true {dockerfile}"
         status = run_command(cmd, log_file)
         results[dockerfile] = status
         if status != 0:
@@ -90,6 +90,7 @@ def main():
         run_command(cmd)
         print("[{}] - {}".format(results[dockerfile], dockerfile))
         sys.stdout.flush()
+        run_command("docker image prune -f")
 
     for dockerfile in dockerfiles:
         if results[dockerfile] == "FAILED":
