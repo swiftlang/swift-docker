534175d3c0728bfb352eae6bcbf004919fef59f7
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-09-04 09:50:02 -0400

    Add license headers to Android build scripts

--- swift-ci/sdks/android/scripts/fetch-source.sh
+++ swift-ci/sdks/android/scripts/fetch-source.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-# Swift Android SDK: Fetch Sources
+# ===----------------------------------------------------------------------===
+#
+#  Swift Android SDK: Fetch Sources
+#
+#  This source file is part of the Swift.org open source project
+#
+#  Copyright (c) 2024 Apple Inc. and the Swift project authors
+#  Licensed under Apache License v2.0 with Runtime Library Exception
+#
+#  See https://swift.org/LICENSE.txt for license information
+#  See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+#
+# ===----------------------------------------------------------------------===
 set -e
 
 # Docker sets TERM to xterm if using a pty; we probably want
